5e62680529160dcd9699330c53c8a666e99f0207
Ignacio Gaute <39140410+IgnacioGaute@users.noreply.github.com>
2025-03-06 21:08:50 -0300

    fix: printer

--- src/printer/printer.service.ts
+++ src/printer/printer.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { exec } from 'child_process';
 import { print } from 'pdf-to-printer';
+import * as os from 'os';
 
 @Injectable()
 export class PrinterService {
   async printFile(filePath: string): Promise<void> {
     try {
-      await print(filePath);
+      const platform = os.platform();
+
+      if (platform === 'win32') {
+        // Para Windows, usa pdf-to-printer
+        await print(filePath);
+      } else if (platform === 'linux' || platform === 'darwin') {
+        // Para Linux y macOS, usa lpr
+        const command = `lpr ${filePath}`;
+        exec(command, (error, stdout, stderr) => {
+          if (error) {
+            console.error('Error al imprimir:', stderr);
+            throw new Error('Error al enviar el archivo a la impresora.');
+          }
+          console.log('Impresión enviada:', stdout);
+        });
+      } else {
+        throw new Error('Sistema operativo no soportado.');
+      }
     } catch (error) {
       console.error('Error al imprimir el archivo:', error);
       throw new Error('Error al enviar el archivo a la impresora.');
